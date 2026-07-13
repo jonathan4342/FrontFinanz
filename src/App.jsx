48831c939c82b9c1f366fda990c318482302e204
Jonathan Osorio <osoriogarrido7@gmail.com>
2026-07-13 18:47:13 -0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div>
+      <h1>Gestión de Clientes y Tickets</h1>
+      <p>Frontend inicializado. Vistas pendientes (Fase 2).</p>
+    </div>
+  )
+}
+
+export default App
